e60b7e752d23a5a0893f35a9604182598d9e3d1e
Jeff Bradberry <jeff.bradberry@gmail.com>
2019-07-22 20:47:38 -0400

    Make clippy happy

--- src/main.rs
+++ src/main.rs
@@ -46,46 +46,46 @@ fn unpack(display: &str) -> (Vec<SquareChoice>,
 
             let b = 3 * (row / 3) + (col / 3);
 
-            state.push(SquareChoice { row: row as u8, col: col as u8, value: value });
+            state.push(SquareChoice { row: row as u8, col: col as u8, value });
             solved_constraints.insert(Constraint::Square { row: row as u8, col: col as u8 });
-            solved_constraints.insert(Constraint::Row { row: row as u8, value: value });
-            solved_constraints.insert(Constraint::Column { col: col as u8, value: value });
-            solved_constraints.insert(Constraint::Block { block: b as u8, value: value });
+            solved_constraints.insert(Constraint::Row { row: row as u8, value });
+            solved_constraints.insert(Constraint::Column { col: col as u8, value });
+            solved_constraints.insert(Constraint::Block { block: b as u8, value });
         }
     }
 
     for row in 0..9u8 {
         for col in 0..9u8 {
             for value in 1..10u8 {
-                let choice = SquareChoice { row: row, col: col, value: value };
+                let choice = SquareChoice { row, col, value };
 
-                let cons_r = Constraint::Row { row: row, value: value };
+                let cons_r = Constraint::Row { row, value };
                 if solved_constraints.contains(&cons_r) { continue; }
 
-                let cons_c = Constraint::Column { col: col, value: value };
+                let cons_c = Constraint::Column { col, value };
                 if solved_constraints.contains(&cons_c) { continue; }
 
                 let b = 3 * (row / 3) + (col / 3);
-                let cons_b = Constraint::Block { block: b, value: value };
+                let cons_b = Constraint::Block { block: b, value };
                 if solved_constraints.contains(&cons_b) { continue; }
 
-                let cons_s = Constraint::Square { row: row, col: col };
+                let cons_s = Constraint::Square { row, col };
                 if solved_constraints.contains(&cons_s) { continue; }
 
                 constraints.entry(cons_r)
-                           .or_insert(Vec::new())
+                           .or_insert_with(Vec::new)
                            .push(choice);
 
                 constraints.entry(cons_c)
-                           .or_insert(Vec::new())
+                           .or_insert_with(Vec::new)
                            .push(choice);
 
                 constraints.entry(cons_b)
-                           .or_insert(Vec::new())
+                           .or_insert_with(Vec::new)
                            .push(choice);
 
                 constraints.entry(cons_s)
-                           .or_insert(Vec::new())
+                           .or_insert_with(Vec::new)
                            .push(choice);
             }
         }
@@ -95,13 +95,13 @@ fn unpack(display: &str) -> (Vec<SquareChoice>,
 }
 
 
-fn pack(state: &Vec<SquareChoice>) -> Vec<String> {
+fn pack(state: &[SquareChoice]) -> Vec<String> {
     let mut output = [['.'; 9]; 9];
 
     for choice in state {
         let row = choice.row as usize;
         let col = choice.col as usize;
-        let value = choice.value as u32;
+        let value = u32::from(choice.value);
 
         output[row][col] = match char::from_digit(value, 10) {
             Some(c) => c,
@@ -128,7 +128,7 @@ fn cover(header: Constraint,
 
     // destructively iterate over column
     for choice in column {
-        let mut row = Row { choice: choice, headers: Vec::new() };
+        let mut row = Row { choice, headers: Vec::new() };
 
         // using the implicit .into_iter would move our constraints ref
         for (other_header, other_col) in constraints.iter_mut() {
@@ -148,16 +148,16 @@ fn cover(header: Constraint,
 
 
 fn uncover(header: Constraint,
-           removals: &Vec<Row>,
+           removals: &[Row],
            constraints: &mut HashMap<Constraint,
                                      Vec<SquareChoice>>) {
     for row in removals {
         constraints.entry(header)
-                   .or_insert(Vec::new())
+                   .or_insert_with(Vec::new)
                    .push(row.choice);
         for &other_header in row.headers.iter() {
             constraints.entry(other_header)
-                       .or_insert(Vec::new())
+                       .or_insert_with(Vec::new)
                        .push(row.choice);
         }
     }
@@ -215,7 +215,6 @@ fn main() {
     let mut input = String::new();
 
     io::stdin().read_to_string(&mut input)
-               .ok()
                .expect("Failed to read input.");
 
     let (mut state, mut constraints) = unpack(&input);
